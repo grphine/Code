cd11ce45bb4947ac4ee156e1608fa095432aaa3d
Juheb <grphine@gmail.com>
2023-02-22 02:19:05 +0000

    added distance test processing

--- Misc/processor.py
+++ Misc/processor.py
@@ -5,19 +5,31 @@ import pandas as pd
 # pd.set_option('display.max_colwidth', None)
 # pd.set_option('display.float_format', '{:20,.2f}'.format)
 
-
-with open('Tests\\Battery\\data\\230214.csv', 'r') as csv_file:
+with open('Tests\\Distance\\second_test.csv', 'r') as csv_file:
     file = list(csv.reader(csv_file, delimiter=','))
     
     print('\\hline')
     print(f'{" & ".join(file[0])} \\\\')
     print('\\hline')
     for k in range(1,5):
-        print(f'{file[k][0]} & {file[k][1]} & {file[k][2]} & \\censor{{{file[k][3]}}} & \\censor{{{file[k][4]}}} & {file[k][5]} & {file[k][6]} & {file[k][7]} \\\\')
-    print(f'\\multicolumn{{8}}{{|c|}}{{$\\cdots$}} \\\\')
+        print(f'{file[k][0]} & {file[k][1]} & {file[k][2]} \\\\')
+    print(f'\\multicolumn{{3}}{{|c|}}{{$\\cdots$}} \\\\')
     for k in range(len(file)-4,len(file)):
-        print(f'{file[k][0]} & {file[k][1]} & {file[k][2]} & \\censor{{{file[k][3]}}} & \\censor{{{file[k][4]}}} & {file[k][5]} & {file[k][6]} & {file[k][7]} \\\\')
+        print(f'{file[k][0]} & {file[k][1]} & {file[k][2]} \\\\')
     print('\hline')
+
+# with open('Tests\\Battery\\data\\230214.csv', 'r') as csv_file:
+#     file = list(csv.reader(csv_file, delimiter=','))
+    
+#     print('\\hline')
+#     print(f'{" & ".join(file[0])} \\\\')
+#     print('\\hline')
+#     for k in range(1,5):
+#         print(f'{file[k][0]} & {file[k][1]} & {file[k][2]} & \\censor{{{file[k][3]}}} & \\censor{{{file[k][4]}}} & {file[k][5]} & {file[k][6]} & {file[k][7]} \\\\')
+#     print(f'\\multicolumn{{8}}{{|c|}}{{$\\cdots$}} \\\\')
+#     for k in range(len(file)-4,len(file)):
+#         print(f'{file[k][0]} & {file[k][1]} & {file[k][2]} & \\censor{{{file[k][3]}}} & \\censor{{{file[k][4]}}} & {file[k][5]} & {file[k][6]} & {file[k][7]} \\\\')
+#     print('\hline')
 
     # tcount = 0
     # line_count = 0
